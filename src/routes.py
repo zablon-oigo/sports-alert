18e7b3fb7e43129b2192f1d40b86f09e4c66b2c9
zablon <zablonoigo51@gmail.com>
2026-07-06 21:02:29 +0300

    delete temp files

--- src/routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter 
-from src.schemas import SubscriptionCreate 
-
-router=APIRouter()
-
-
-@router.post("/")
-def subscribe(payload: SubscriptionCreate):
-    return {"message:" "Subscription Createed"}
