b8655f04d55cfa1148ef51d7ab7820f6ba047243
zablon <zablonoigo51@gmail.com>
2026-07-11 20:14:30 +0300

    producer

---
+++ src/main/java/bike/Producer.java
@@ -0,0 +1,70 @@
+package bike;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class Producer {
+
+    private final KafkaProducer<String, String> producer;
+    private final String topic;
+    private final HttpClientService httpClient;
+
+    public Producer(String bootstrapServers, String topic) {
+
+        this.topic = topic;
+        this.httpClient = new HttpClientService();
+
+        Properties props = new Properties();
+
+        props.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers);
+
+        props.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+
+        props.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+
+        producer = new KafkaProducer<>(props);
+    }
+
+    public void publishBikeData() {
+
+        String xml = httpClient.fetchBikeData();
+
+        ProducerRecord<String, String> record =
+                new ProducerRecord<>(topic, xml);
+
+        producer.send(record, (metadata, exception) -> {
+
+            if (exception == null) {
+
+                System.out.println(
+                        "Sent to topic=" + metadata.topic() +
+                        " partition=" + metadata.partition() +
+                        " offset=" + metadata.offset());
+
+            } else {
+
+                exception.printStackTrace();
+
+            }
+
+        });
+
+    }
+
+    public void close() {
+
+        producer.flush();
+        producer.close();
+
+    }
+}
