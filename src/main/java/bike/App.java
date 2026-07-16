891c8063308a0f0375bdeaa584dea7a6fdd962c5
zablon <zablonoigo51@gmail.com>
2026-07-16 10:33:26 +0300

    load avro schema

--- src/main/java/bike/App.java
+++ src/main/java/bike/App.java
@@ -1,28 +1,27 @@
 package bike;
 
+import java.io.File;
+
+import org.apache.avro.Schema;
+
 public class App {
 
     private static final String BOOTSTRAP = "localhost:29092";
     private static final String TOPIC = "bike.analytics";
 
     public static void main(String[] args) {
-
-        Producer producer = new Producer(BOOTSTRAP, TOPIC);
-
         try {
 
-            producer.publishBikeData();
+            Schema.Parser parser = new Schema.Parser();
+            Schema schema = parser.parse(new File("src/main/avro/BikeStation.avsc"));
 
+            Producer producer = new Producer(BOOTSTRAP, TOPIC, schema);
+
+            producer.publishBikeData();
             System.out.println("Finished publishing bike stations.");
 
         } catch (Exception e) {
-
             e.printStackTrace();
-
-        } finally {
-
-            producer.close();
-
         }
     }
 }
